1a8d5d2c9bcc082b978231ffdc34c2140f813ca7
WillAxe <wilaxe1129@gmail.com>
2025-10-24 12:10:33 +0200

    fixed route for planetsview

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "./views/HomeView.vue"
-import PlanetsView from "./views/planetsView.vue"
+import PlanetsView from "./views/PlanetsView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
